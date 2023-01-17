dea5007826be42c21acdd5c5c6cb6721d11eb33c
etiger314 <etiger314@hotmail.com>
2023-01-17 14:06:20 -0500

    Creation fichiers

---
+++ Code/Moteur.h
@@ -0,0 +1,10 @@
+/***
+ * @author Étienne Germain
+ * @file Moteur.h
+ * @date 17 janvier 2023
+***/
+
+class Moteur
+{
+
+};
---
+++ Code/Robot.h
@@ -0,0 +1,10 @@
+/***
+ * @author Étienne Germain
+ * @file Robot.h
+ * @date 17 janvier 2023
+***/
+
+class Robot
+{
+
+};
